a5ed348d5c3573d6a079a2e5f66e94115c1ce961
David Hirtle <david.hirtle@pulseenergy.com>
2013-07-11 12:16:50 -0700

    add done callback useful when used programmatically, eg via grunt

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var m = require('./lib/nohm-migrations');
-
-module.exports = {
-	migrate: function (key, redis_port, redis_host, prefix, nohmInstance) {
-		m.migrate(key, redis_port, redis_host, prefix, nohmInstance);
-	}
-}
+module.exports = require('./lib/nohm-migrations');
